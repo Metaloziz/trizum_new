583b6da4742c72ce6648b20160727c25db1560c4
User <vitalik.kamyshev.1997@mail.ru>
2022-03-29 20:43:01 +0300

    create page teacherMain

--- src/pages/scenario/index.page.tsx
+++ src/pages/scenario/index.page.tsx
@@ -2,14 +2,17 @@ import BlockShulte from '@components/block-shulte/BlockShulte';
 import ButtonPlay from '@components/button-play/ButtonPlay';
 import Modal from '@components/modal/Modal';
 import PagePersonalData from '@components/page-persona-data/PagePersonalData';
+import TeacherMainItem from '@components/teacher-main/teacher-main-item/TeacherMainItem';
+import TeacherMain from '@components/teacher-main/TeacherMain';
 
 const Tablet = () => {
   return (
     <div>
-      <PagePersonalData />
-      <Modal />
-      <ButtonPlay />
-      <BlockShulte />
+      {/*<PagePersonalData />*/}
+      {/*<Modal />*/}
+      {/*<ButtonPlay />*/}
+      {/*<BlockShulte />*/}
+      <TeacherMain />
     </div>
   );
 };

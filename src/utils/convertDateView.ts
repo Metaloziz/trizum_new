e0a084f3743320fca3ee1570ec6523220ae3562c
Andrew Gaityukevich <andrewgaity@yandex.by>
2022-08-30 19:30:26 +0300

    #24 feat: filter for users

--- src/utils/convertDateView.ts
+++
@@ -1,8 +0,0 @@
-export const convertDateView = (date: string) => {
-  // todo for filter
-  const draft = date.slice(0, 16).split(' '); // 16 это индекс последней минуты
-
-  draft[0] = draft[0].replaceAll('-', '/').split('/').reverse().join('/');
-
-  return draft.join(' ');
-};

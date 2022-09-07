2608f7cde2cb080423fc436d37f2a7586a664b2b
Aliaksandr Novik <navaliny1989@gmail.com>
2022-09-07 14:22:55 +0300

    fix: 5 bug of the list (block-unblock user)

--- src/components/button-open-close/SetStatusButton.tsx
+++ src/components/button-open-close/SetStatusButton.tsx
@@ -9,21 +9,20 @@ import { ResponseUserT, UserStatusT } from 'app/types/UserTypes';
 import iconClosed from 'assets/svgs/closed-lock.svg';
 import iconOpen from 'assets/svgs/open-lock.svg';
 import Image from 'components/image/Image';
+import usersStore from 'app/stores/usersStore';
 
 type Props = Pick<ResponseUserT, 'status' | 'id'>;
 
 const SetStatusButton: FC<Props> = ({ status, id }) => {
-  const [isActive, setIsActive] = useState<boolean>(status === 'active');
+  const [isActive, setIsActive] = useState<boolean>(status);
   const [isDisable, setIsDisable] = useState<boolean>(false);
-
+  
   const setNewStatus = async () => {
     setIsDisable(true);
 
     try {
       setIsActive(!isActive);
-
-      const newStatus = isActive ? 'active' : 'blocked';
-      const res = await usersService.updateUser({ status: newStatus }, id);
+      const res = await usersStore.updateUser({isActive:!isActive}, id)
       setIsDisable(false);
     } catch (e) {
       setIsDisable(false);
@@ -31,20 +30,19 @@ const SetStatusButton: FC<Props> = ({ status, id }) => {
   };
 
   const iconButton = isActive ? (
-    <Image src={iconOpen} alt="open" width={16} height={20} />
-  ) : (
     <Image src={iconClosed} alt="lock" width={16} height={20} />
+  ) : (
+    <Image src={iconOpen} alt="open" width={16} height={20} />
   );
-
   return (
     <button
       type="button"
-      className={cn(styles.Button, !isActive && styles.closed, isDisable && styles.disable)}
+      className={cn(styles.Button, isActive && styles.closed, isDisable && styles.disable)}
       onClick={setNewStatus}
       disabled={isDisable}
     >
       <span className={styles.icon}>{iconButton}</span>
-      {isActive ? 'Разблокировать' : 'Заблокировать'}
+      {isActive ? 'Заблокировать' : 'Разблокировать'}
     </button>
   );
 };

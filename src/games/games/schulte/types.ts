505d4d3e50c45016f548e418d8c25717a3671784
Zarazaka Mikalai <golmtz@gmail.com>
2022-09-13 14:05:04 +0300

    fixed styles for adaptive game page

--- src/games/games/schulte/types.ts
+++ src/games/games/schulte/types.ts
@@ -1,20 +1,22 @@
-import { GameProps, GameResult } from '../../common/types';
+import {GameProps, GameResult} from '../../common/types';
 
 export interface Props extends GameProps {
-  sizeX?: number;
-  sizeY?: number;
-  colors?: number;
-  colorsMap?: string[];
-  onEnd(result?: GameResult): void;
+    sizeX?: number;
+    sizeY?: number;
+    colors?: number;
+    colorsMap?: string[];
+
+    onEnd(result?: GameResult): void;
 }
 
 const PropsDefault: Props = {
-  sizeX: 3,
-  sizeY: 3,
-  colors: 1,
-  colorsMap: ['#333', '#f00', '#0f0'],
-  width: 200,
-  onEnd: () => {},
+    sizeX: 3,
+    sizeY: 3,
+    colors: 1,
+    colorsMap: ['#333', '#f00', '#0f0'],
+    width: 200,
+    onEnd: () => {
+    },
 };
 
-export { PropsDefault };
+export {PropsDefault};
